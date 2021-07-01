1fd9d7ce885c72eb2c1a35a8bd2570dbccb05d2b
Uptimum <uptimumdn2000@gmail.com>
2021-07-01 19:04:54 +0700

    fix code

--- src/controllers/drive-stream.controller.js
+++ src/controllers/drive-stream.controller.js
@@ -6,55 +6,28 @@ const { decrypt } = require('../lib/mahoa')
 const getLinkStream = async (req, res, next) => {
     const id = req.query.id
     //lấy dữ liệu từ cache
-    const cookieStream = await getAsync(`${id}_cookie`)
-    const urlStream = await getAsync(`${id}_urlStream`)
-    // nếu tồn tại thì nhảy tiếp
-    if (cookieStream && urlStream) {
-        req.cookieStream = cookieStream
-        req.urlStream = urlStream
-        return next()
-    }
-    // không thì tìm nạp
+    let cookieStream = await getAsync(`cookie`)
+    let urlStream = await getAsync(`${id}_urlStream`)
+    let idMovieStream
+
+    if (cookieStream) req.cookieStream = cookieStream
     else {
-        const idMovieStream = decrypt(id)
-        const cookieStream = await fetch(
-            `https://drive.google.com/u/3/get_video_info?docid=${idMovieStream}`
-        ).then(
-            (response) => response.headers.raw()['set-cookie'][0].split('; ')[0]
-        )
-
-        const opts = {
-            headers: {
-                cookie: cookieStream,
-            },
-        }
-        await fetch(
-            `https://drive.google.com/u/3/get_video_info?docid=${idMovieStream}`,
-            opts
-        )
-            .then((response) => response.text())
-            .then(async (body) => {
-                const data = queryString.parse(body)
-                const arrUrl = data.fmt_stream_map.split(',')
-                //lấy chất lượng cao nhất
-                const url = arrUrl[arrUrl.length - 1].split('|')[1]
-                //lưu url vào cache
-                client_redis.setex(
-                    `${id}_cookie`,
-                    60 * 60 * 3 - 10 * 60,
-                    cookieStream
-                )
-                client_redis.setex(
-                    `${id}_urlStream`,
-                    60 * 60 * 3 - 10 * 60,
-                    url
-                )
-
-                req.cookieStream = cookieStream
-                req.urlStream = url
-            })
-        return next()
+        idMovieStream = decrypt(id)
+        cookieStream = await getCookie(idMovieStream)
+        console.log('cookieStream ', cookieStream)
+        client_redis.setex(`cookie`, 60 * 60 * 3 - 10 * 60, cookieStream)
+        req.cookieStream = cookieStream
     }
+    // nếu url tồn tại thì gán req.urlStream = urlStream
+    if (urlStream) {
+        req.urlStream = urlStream
+    } else {
+        if (idMovieStream == undefined) idMovieStream = decrypt(id)
+        urlStream = await getUrlStream(idMovieStream, cookieStream)
+        client_redis.setex(`${id}_urlStream`, 60 * 60 * 3 - 10 * 60, urlStream)
+        req.urlStream = urlStream
+    }
+    return next()
 }
 
 const streamVideo = async (req, res) => {
@@ -85,4 +58,43 @@ const streamVideo = async (req, res) => {
 module.exports = {
     getLinkStream,
     streamVideo,
+}
+
+function getUrlStream(idMovieStream, cookieStream) {
+    return new Promise(async (resolve, reject) => {
+        const opts = {
+            headers: {
+                cookie: cookieStream,
+            },
+        }
+        await fetch(
+            `https://drive.google.com/u/7/get_video_info?docid=${idMovieStream}`,
+            opts
+        )
+            .then((response) => response.text())
+            .then(async (body) => {
+                const data = queryString.parse(body)
+                console.log(data)
+                const arrUrl = data.fmt_stream_map.split(',')
+                //lấy chất lượng cao nhất
+                const url = arrUrl[arrUrl.length - 1].split('|')[1]
+
+                return resolve(url)
+            })
+            .catch((err) => reject(null))
+    })
+}
+
+function getCookie(idMovieStream) {
+    return new Promise(async (resolve, reject) => {
+        await fetch(
+            `https://drive.google.com/u/7/get_video_info?docid=${idMovieStream}`
+        )
+            .then((response) =>
+                resolve(response.headers.raw()['set-cookie'][0].split('; ')[0])
+            )
+            .catch((err) => reject(null))
+        // client_redis.setex(`cookie`, 60 * 60 * 3 - 10 * 60, cookieStream)
+        // req.cookieStream = cookieStream
+    })
 }
